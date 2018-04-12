3be33fde7fb70ffa2ec1ff5b8e39f8b423a3c55a
Ghananeel Rotithor <grotithor@ufl.edu>
2018-04-12 14:17:19 +0530

    Update and rename KLMS.m to Kernel_lms.m

--- Kernel_lms.m
+++ Kernel_lms.m
@@ -1,13 +1,14 @@
-function y=KLMS(u,d,m)
+function y = Kernel_lms(u,d,m)
+% Learning Rate for KLMS
 lr_k = .3;
 %   lr_k = .6;
 
 N_tr=length(u);
 
 %init
-e_k = zeros(N_tr,1);
-y = zeros(N_tr,1);
-mse_te_k = zeros(N_tr,1);
+e_k = zeros(N_tr,1);        % Error
+y = zeros(N_tr,1);          % Predicted Output
+mse_te_k = zeros(N_tr,1);   % Mean Squared Error
 
 T=d(1:N_tr-m+1);
 
@@ -19,12 +20,15 @@ end
 e_k(1) = T(1);
 y(1) = 0;
 
-% start
+% start KLMS
 for n=2:N_tr-m+1
-    %training
+    % training
     ii = 1:n-1;
+    % Compute the predicted output in Gaussian Kernel Space
     y(n) = lr_k*e_k(ii)'*(exp(-sum((X(:,n)*ones(1,n-1)-X(:,ii)).^2)))';
+    % Compute Error between true and predicted output
     e_k(n) = T(n) - y(n);
+    % Compute Normalized Mean-Squared Error
     nmse(n)=(e_k(n)*e_k(n))/(u(:,n)*u(:,n)'+0.01);
     
 end
